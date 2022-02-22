211d26ae26c6089276517fd5e4956f935d2a41da
Amanda Lin <amanda.lin.aut@gmail.com>
2022-02-21 23:40:56 -0500

    Fixed module errors; added styling to account tab; added route to props for account and to do details

--- App.js
+++ App.js
@@ -1,39 +1,48 @@
-import React, { Component } from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { StyleSheet, Text, View,Button,TextInput,SafeAreaView} from 'react-native';
+import React, { Component } from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  TextInput,
+  SafeAreaView,
+} from "react-native";
+import { Ionicons } from "@expo/vector-icons";
 // Navigation imports
-import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
-import {createStackNavigator} from '@react-navigation/stack';
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { createStackNavigator } from "@react-navigation/stack";
 // Component imports
-import ToDoInput from './screens/ToDoInput.js'
-import Account from './screens/Account.js'
+import ToDoInput from "./screens/ToDoInput.js";
+import ToDoDetails from "./screens/ToDoDetails.js";
+import Account from "./screens/Account.js";
 
 // Initialize Navigations
 const Tab = createBottomTabNavigator();
 const Stack = createStackNavigator();
 
 class ToDoStack extends Component {
-  reder() {
+  render() {
     return (
       <Stack.Navigator>
-        <Stack.Screen 
+        <Stack.Screen
           name="toDoInput"
-          component={ ToDoInput }
+          component={ToDoInput}
           screenOptions={{
-            headerTintColor: 'white',
-            headerStyle: { backgroundColor: 'blue'}
+            headerTintColor: "white",
+            headerStyle: { backgroundColor: "blue" },
           }}
         />
 
         <Stack.Screen
           name="toDoDetails"
-          component={ ToDoDetails }
+          component={ToDoDetails}
           options={{
-            title: 'To-Do Details'
+            title: "To-Do Details",
           }}
         />
       </Stack.Navigator>
-    )
+    );
   }
 }
 
@@ -46,41 +55,40 @@ export default class App extends Component {
             tabBarIcon: ({ focused, color, size }) => {
               let iconName;
 
-              if (route.name === 'toDoTab') {
-                iconName = focused ? 'home' : 'home-outline';
-              } else if (route.name === 'accountTab') {
-                iconName = focused ? 'person-circle' : 'person-circle-outline';
+              if (route.name === "To-Dos") {
+                iconName = focused ? "home" : "home-outline";
+              } else if (route.name === "Account") {
+                iconName = focused ? "person-circle" : "person-circle-outline";
               }
 
               return <Ionicons name={iconName} size={size} color={color} />;
-            }
+            },
           })}
           tabBarOptions={{
-            activeTintColor: 'blue',
-            inactiveTintColor: 'gray'
+            activeTintColor: "blue",
+            inactiveTintColor: "black",
           }}
         >
-          <Tab.Screen name='toDoTab' component={ ToDoInput } />
-          <Tab.Screen name='accounTabt' component={ Account } />
+          <Tab.Screen name="To-Dos" component={ToDoInput} />
+          <Tab.Screen name="Account" component={Account} />
         </Tab.Navigator>
       </NavigationContainer>
-    )
+    );
   }
 }
 
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    paddingTop: Constants.statusBarHeight,
-    backgroundColor: '#ecf0f1',
-    padding: 8,
-  },
-  paragraph: {
-    margin: 24,
-    fontSize: 18,
-    fontWeight: 'bold',
-    textAlign: 'center',
-  },
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     justifyContent: "center",
+//     paddingTop: Constants.statusBarHeight,
+//     backgroundColor: "#ecf0f1",
+//     padding: 8,
+//   },
+//   paragraph: {
+//     margin: 24,
+//     fontSize: 18,
+//     fontWeight: "bold",
+//     textAlign: "center",
+//   },
+// });
---
+++ screens/ToDoDetails.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import { Button, Text, View, StyleSheet } from "react-native";
+
+export default class ToDoDetails extends Component {
+  render() {
+    const { navigation, route } = this.props;
+    let { toDoTitle, toDoStatus, toDoBody } = route.params;
+    return (
+      <View>
+        <Text>To Do Details</Text>
+      </View>
+    );
+  }
+}
